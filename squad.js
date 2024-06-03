643b47677ff4bd3924434af038dd48b1991689e2
Premkumar <117425494+premcrazy@users.noreply.github.com>
2024-06-03 16:10:43 +0530

    Add files via upload

---
+++ squad.js
@@ -0,0 +1,12 @@
+var viewbar = document.querySelector(".side-navbar")
+
+function showNavbar()
+{
+    
+    viewbar.style.left="0"
+}
+function closeNavbar()
+{
+  
+    viewbar.style.left="-65%"
+}
